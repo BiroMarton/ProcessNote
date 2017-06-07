b309fe2e2a6e64191ef5ce65f5ec704945b97773
BiroMarton <rezoner9@gmail.com>
2017-06-07 20:47:52 +0200

    Added cpu usage monitoring and note.

--- ProcessNote/ProcessNote/Form1.Designer.cs
+++ ProcessNote/ProcessNote/Form1.Designer.cs
@@ -35,37 +35,39 @@
             this.Num = new System.Windows.Forms.Label();
             this.Memory = new System.Windows.Forms.Label();
             this.MemoryNum = new System.Windows.Forms.Label();
-            this.ProcssStart = new System.Windows.Forms.Label();
-            this.StartNum = new System.Windows.Forms.Label();
             this.Note = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.NoteSave = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CPUNum = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Processes
             // 
             this.Processes.FormattingEnabled = true;
-            this.Processes.ItemHeight = 16;
-            this.Processes.Location = new System.Drawing.Point(54, 72);
+            this.Processes.Location = new System.Drawing.Point(40, 58);
+            this.Processes.Margin = new System.Windows.Forms.Padding(2);
             this.Processes.Name = "Processes";
-            this.Processes.Size = new System.Drawing.Size(300, 212);
+            this.Processes.Size = new System.Drawing.Size(226, 173);
             this.Processes.TabIndex = 0;
             this.Processes.SelectedIndexChanged += new System.EventHandler(this.Processes_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 43);
+            this.label1.Location = new System.Drawing.Point(38, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.Size = new System.Drawing.Size(98, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Running processes";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(370, 72);
+            this.button1.Location = new System.Drawing.Point(278, 58);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 26);
+            this.button1.Size = new System.Drawing.Size(87, 21);
             this.button1.TabIndex = 2;
             this.button1.Text = "Refresh list";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,93 +76,100 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(554, 72);
+            this.label2.Location = new System.Drawing.Point(416, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.Size = new System.Drawing.Size(104, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Running processes: ";
             // 
             // Num
             // 
             this.Num.AutoSize = true;
-            this.Num.Location = new System.Drawing.Point(698, 72);
+            this.Num.Location = new System.Drawing.Point(524, 58);
+            this.Num.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Num.Name = "Num";
-            this.Num.Size = new System.Drawing.Size(46, 17);
+            this.Num.Size = new System.Drawing.Size(35, 13);
             this.Num.TabIndex = 4;
             this.Num.Text = "label3";
             // 
             // Memory
             // 
             this.Memory.AutoSize = true;
-            this.Memory.Location = new System.Drawing.Point(444, 171);
+            this.Memory.Location = new System.Drawing.Point(286, 137);
+            this.Memory.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Memory.Name = "Memory";
-            this.Memory.Size = new System.Drawing.Size(105, 17);
+            this.Memory.Size = new System.Drawing.Size(79, 13);
             this.Memory.TabIndex = 5;
             this.Memory.Text = "Memory usage:";
             // 
             // MemoryNum
             // 
             this.MemoryNum.AutoSize = true;
-            this.MemoryNum.Location = new System.Drawing.Point(556, 170);
+            this.MemoryNum.Location = new System.Drawing.Point(370, 136);
+            this.MemoryNum.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.MemoryNum.Name = "MemoryNum";
-            this.MemoryNum.Size = new System.Drawing.Size(0, 17);
+            this.MemoryNum.Size = new System.Drawing.Size(0, 13);
             this.MemoryNum.TabIndex = 6;
-            // 
-            // ProcssStart
-            // 
-            this.ProcssStart.AutoSize = true;
-            this.ProcssStart.Location = new System.Drawing.Point(444, 192);
-            this.ProcssStart.Name = "ProcssStart";
-            this.ProcssStart.Size = new System.Drawing.Size(115, 17);
-            this.ProcssStart.TabIndex = 7;
-            this.ProcssStart.Text = "Process started: ";
-            // 
-            // StartNum
-            // 
-            this.StartNum.AutoSize = true;
-            this.StartNum.Location = new System.Drawing.Point(557, 192);
-            this.StartNum.Name = "StartNum";
-            this.StartNum.Size = new System.Drawing.Size(0, 17);
-            this.StartNum.TabIndex = 8;
             // 
             // Note
             // 
             this.Note.AcceptsReturn = true;
-            this.Note.Location = new System.Drawing.Point(54, 323);
+            this.Note.Location = new System.Drawing.Point(40, 262);
+            this.Note.Margin = new System.Windows.Forms.Padding(2);
             this.Note.Multiline = true;
             this.Note.Name = "Note";
-            this.Note.Size = new System.Drawing.Size(300, 194);
+            this.Note.Size = new System.Drawing.Size(226, 158);
             this.Note.TabIndex = 9;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 303);
+            this.label3.Location = new System.Drawing.Point(38, 246);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(264, 17);
+            this.label3.Size = new System.Drawing.Size(196, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Notes (Dada is only stored temporarily!!!)";
             // 
             // NoteSave
             // 
-            this.NoteSave.Location = new System.Drawing.Point(361, 493);
+            this.NoteSave.Location = new System.Drawing.Point(271, 401);
+            this.NoteSave.Margin = new System.Windows.Forms.Padding(2);
             this.NoteSave.Name = "NoteSave";
-            this.NoteSave.Size = new System.Drawing.Size(75, 23);
+            this.NoteSave.Size = new System.Drawing.Size(56, 19);
             this.NoteSave.TabIndex = 11;
             this.NoteSave.Text = "Save";
             this.NoteSave.UseVisualStyleBackColor = true;
             this.NoteSave.Click += new System.EventHandler(this.NoteSave_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(286, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "CPU usage:";
+            // 
+            // CPUNum
+            // 
+            this.CPUNum.AutoSize = true;
+            this.CPUNum.Location = new System.Drawing.Point(356, 150);
+            this.CPUNum.Name = "CPUNum";
+            this.CPUNum.Size = new System.Drawing.Size(0, 13);
+            this.CPUNum.TabIndex = 13;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 612);
+            this.ClientSize = new System.Drawing.Size(756, 497);
+            this.Controls.Add(this.CPUNum);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.NoteSave);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.Note);
-            this.Controls.Add(this.StartNum);
-            this.Controls.Add(this.ProcssStart);
             this.Controls.Add(this.MemoryNum);
             this.Controls.Add(this.Memory);
             this.Controls.Add(this.Num);
@@ -168,6 +177,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Processes);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -184,11 +194,11 @@
         private System.Windows.Forms.Label Num;
         private System.Windows.Forms.Label Memory;
         private System.Windows.Forms.Label MemoryNum;
-        private System.Windows.Forms.Label ProcssStart;
-        private System.Windows.Forms.Label StartNum;
         private System.Windows.Forms.TextBox Note;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button NoteSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label CPUNum;
     }
 }
 
--- ProcessNote/ProcessNote/Form1.cs
+++ ProcessNote/ProcessNote/Form1.cs
@@ -14,22 +14,39 @@ namespace ProcessNote
     public partial class Form1 : Form
     {
         Dictionary<string, string> notes = new Dictionary<string, string>();
+        
+
+        private static float RunTest(string appName)
+        {
+           float result = 0;
+           PerformanceCounter total_cpu = new PerformanceCounter("Process", "% Processor Time", "_Total");
+           PerformanceCounter process_cpu = new PerformanceCounter("Process", "% Processor Time", appName);
+
+          total_cpu.NextValue();
+          process_cpu.NextValue();
+          System.Threading.Thread.Sleep(1000);
+          float t = total_cpu.NextValue();
+          float p = process_cpu.NextValue();
+          result = p / t * 100;
+         
+                
+          return result;
+        }
+
         public Form1()
         {
+         
             InitializeComponent();
             Process[] process = Process.GetProcesses();
-            ListProcesses();
-            Num.Text = process.Length.ToString();
-            
+            this.TopMost = true;
+           
 
-
-             void ListProcesses()
+            foreach (Process p in process)
             {
-                foreach (Process p in process)
-                {
-                    Processes.Items.Add(p.ProcessName);
-                }
+                Processes.Items.Add(p.ProcessName);
             }
+            ;
+            Num.Text = process.Length.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -49,7 +66,10 @@ namespace ProcessNote
             String name = Processes.SelectedItem.ToString();
             Process[] proc = Process.GetProcessesByName(name);
             MemoryNum.Text = (proc[0].PrivateMemorySize64 / (1024 * 1024)).ToString() + " MB";
-            StartNum.Text = proc[0].StartTime.ToString();
+            notes.TryGetValue(name, out string o);
+            Note.Text = o;
+            CPUNum.Text = RunTest(name).ToString() + "%";
+
         }
 
         private void NoteSave_Click(object sender, EventArgs e)
@@ -58,9 +78,15 @@ namespace ProcessNote
             String content = Note.Text;
             if (notes.ContainsKey(name))
             {
-             
+                notes[name] = content;
+            }
+            else
+            {
+                notes.Add(name, content);
             }
 
         }
+
+
     }
 }
